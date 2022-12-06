cb905819110812983e19b704efffb2542d528d9e
ReyMagos <reymagos838@gmail.com>
2022-12-06 20:53:56 +0500

    optimize.py

--- src/optimize.py
+++ src/optimize.py
@@ -2,13 +2,6 @@ import re
 from collections import deque
 
 
-class JsonObject:
-    NUMBER = 0
-    STRING = 1
-    MAP = 2
-    ARRAY = 3
-
-
 def optimize(
         src_path, dest_path,
         remove_whitespaces=True,  # Remove all whitespace characters
@@ -16,39 +9,78 @@ def optimize(
         merge_rounds=1,           # Number of merging rounds
         merge_max_distance=5      # Max distance within points can be merged
 ):
-    standalone = ",:"
-    dual = {"{": "}", "[": "]", "\"": "\""}
+    class Json:
+        STRING = 1
+        MAP = 2
+        ARRAY = 3
+
+        KEY = 4
+        VALUE = 5
 
     stack = deque()
+    waiting_for = Json.VALUE
     namespace = deque()
+    value_buffer = None
 
     with open(src_path) as src, open(dest_path, mode="a") as dest:
-        while char := src.read(1):
-            if re.match(r"features.*.geometry.coordinates", ".".join(namespace)):
+
+        def copying_reader():
+            nonlocal value_buffer, waiting_for
+
+            while char := src.read(1):
+                if value_buffer is not None:
+                    value_buffer += char
+                elif char == '[':
+                    stack.append(Json.ARRAY)
+                    namespace.append("0")
+                elif char == '{':
+                    stack.append(Json.MAP)
+                    waiting_for = Json.KEY
+                elif char in "]}":
+                    stack.pop()
+                elif char == '"':
+                    if stack[-1] == Json.STRING:
+                        stack.pop()
+                        if waiting_for == Json.KEY:
+                            namespace.append(value_buffer)
+                            value_buffer = None
+                    else:
+                        if waiting_for == Json.KEY:
+                            value_buffer = ""
+                        stack.append(Json.STRING)
+                elif char == ':':
+                    waiting_for = Json.VALUE
+
+                    # Parse coordinates to do optimizations
+                    if re.match(r"features.*.geometry.coordinates", ".".join(namespace)):
+                        dest.write(':')
+                        return parsing_reader
+
+                elif char == ',':
+                    if stack[-1] == Json.ARRAY:
+                        namespace[-1] = str(int(namespace[-1]) + 1)
+                    elif stack[-1] == Json.MAP:
+                        waiting_for = Json.KEY
+
+                if not char.isspace() or not remove_whitespaces:
+                    dest.write(char)
+
+            return None
+
+        def parsing_reader():
+            while char := src.read(1):
                 ...
 
-            if char.isspace():
-                ...
-            if char.isdigit():
-                ...
-            if char.isalpha():
-                if stack[-1] == JsonObject.STRING:
-                    ...
-                else:
-                    raise ValueError("Letters outside of string")
-            if char == "," and stack[-1] == JsonObject.ARRAY:
-                namespace[-1] = str(int(namespace[-1]) + 1)
-            if char == "\"":
-                if stack[-1] == JsonObject.STRING:
-                    ...
-                else:
-                    stack.append(JsonObject.STRING)
-            if char == "{":
-                stack.append(JsonObject.MAP)
-            if char == "[":
-                stack.append(JsonObject.ARRAY)
-                namespace.append("0")
+        def merge_points(geometry):
+            ...
+
+        def adjust_precision(geometry):
+            for ring in geometry:
+                for point in ring:
+                    point[0] = ...
+                    point[1] = ...
 
 
-
-
+        reader = copying_reader
+        while (next_reader := reader()) is not None:
+            reader = next_reader
