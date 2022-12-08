3dd92d7b532824af889396a2e298a423d0abbe1d
ReyMagos <reymagos838@gmail.com>
2022-12-08 08:39:28 +0500

    DONT RUN THIS CODE

--- src/optimize.py
+++ src/optimize.py
@@ -1,5 +1,8 @@
 import re
 from collections import deque
+from dataclasses import dataclass
+from decimal import Decimal
+from typing import Any, Iterable
 
 
 def optimize(
@@ -10,6 +13,7 @@ def optimize(
         merge_max_distance=5      # Max distance within points can be merged
 ):
     class Json:
+        NUMBER = 0
         STRING = 1
         MAP = 2
         ARRAY = 3
@@ -17,69 +21,135 @@ def optimize(
         KEY = 4
         VALUE = 5
 
-    stack = deque()
-    waiting_for = Json.VALUE
-    namespace = deque()
-    value_buffer = None
+    @dataclass
+    class JsonObj:
+        entity: int | None
+        key: str | None
+        value: Any | None
+
+        @staticmethod
+        def empty():
+            return JsonObj(None, None, None)
+
+    class JsonStack:
+        def __init__(self):
+            self.entity_stack = deque()
+            self.key_stack = deque()
+
+        def __add_to_stack(self, stack, elements):
+            if elements is not None:
+                if isinstance(elements, Iterable):
+                    for element in elements:
+                        stack.append(element)
+                else:
+                    stack.append(elements)
+
+        def push(self, entities=None, keys=None):
+            if entities is not None:
+                if isinstance(entities, Iterable):
+                    for
+                    self.entity_stack.append(*entities)
+
+        def pop(self, entities=None, keys=None):
+            return self.stack.pop()
+
+        def top(self):
+            if len(self.stack) == 0:
+                return None
+            return self.stack[-1]
+
+        def path(self):
+            return ".".join([obj[1] for obj in self.stack])
+
+    stack = JsonStack()
+    value_buffer = ""
 
     with open(src_path) as src, open(dest_path, mode="a") as dest:
 
         def copying_reader():
-            nonlocal value_buffer, waiting_for
+            nonlocal value_buffer
 
             while char := src.read(1):
-                if value_buffer is not None:
+                if char == '"':
+                    if stack.top().entity == Json.STRING:
+                        if stack.top().key is None:
+                            # String without key is key itself
+                            stack.modify(key=value_buffer, entity=None)
+                            value_buffer = ""
+                    else:
+                        stack.tell(entity=Json.STRING)
+                elif stack.top().entity == Json.STRING:
                     value_buffer += char
                 elif char == '[':
-                    stack.append(Json.ARRAY)
-                    namespace.append("0")
+                    stack.tell(entity=Json.ARRAY, key="0")
                 elif char == '{':
-                    stack.append(Json.MAP)
-                    waiting_for = Json.KEY
+                    stack.tell(entity=Json.MAP)
                 elif char in "]}":
-                    stack.pop()
-                elif char == '"':
-                    if stack[-1] == Json.STRING:
-                        stack.pop()
-                        if waiting_for == Json.KEY:
-                            namespace.append(value_buffer)
-                            value_buffer = None
-                    else:
-                        if waiting_for == Json.KEY:
-                            value_buffer = ""
-                        stack.append(Json.STRING)
+                    # remove
+                    ...
                 elif char == ':':
-                    waiting_for = Json.VALUE
 
-                    # Parse coordinates to do optimizations
-                    if re.match(r"features.*.geometry.coordinates", ".".join(namespace)):
+                    # Parse geometry coordinates to do optimizations
+                    if re.match(r"features.*.geometry.coordinates", stack.path()):
                         dest.write(':')
-                        return parsing_reader
+                        return geometry_reader
 
                 elif char == ',':
-                    if stack[-1] == Json.ARRAY:
-                        namespace[-1] = str(int(namespace[-1]) + 1)
-                    elif stack[-1] == Json.MAP:
-                        waiting_for = Json.KEY
+                    if stack.top().entity == Json.ARRAY:
+                        stack.modify(key=str(int(stack.top().key) + 1))
+                    elif stack.top().entity == Json.MAP:
+                        stack.tell(key=None)
 
                 if not char.isspace() or not remove_whitespaces:
                     dest.write(char)
 
             return None
 
-        def parsing_reader():
+        def geometry_reader():
+            nonlocal value_buffer
+            geometry = []
+            current_array = None
+
             while char := src.read(1):
-                ...
+                if char.isdigit() or char == '.':
+                    value_buffer += char
+                elif char == ',':
+                    if stack.top().entity == Json.ARRAY:
+                        namespace[-1] = str(int(namespace[-1]) + 1)
+                        if value_buffer:
+                            current_array.append(Decimal(value_buffer))
+                            value_buffer = ""
+
+                    # End of geometry
+                    elif stack.top() == Json.MAP:
+                        namespace.pop()
+
+                        for _ in range(merge_rounds):
+                            merge_points(geometry)
+
+                        dest.write(str(geometry))
+
+                        # TODO: adjust precision, remove or leave whitespaces
+
+                        return copying_reader
+
+                elif char == '[':
+                    stack.tell(entity=Json.ARRAY)
+                    if current_array is None:
+                        geometry = []
+                        current_array = geometry
+                    else:
+                        new_array = []
+                        current_array.append(new_array)
+                        current_array = new_array
+                elif char == ']':
+                    stack.pop()
+                    if value_buffer:
+                        current_array.append(Decimal(value_buffer))
+                        value_buffer = ""
 
         def merge_points(geometry):
             ...
-
-        def adjust_precision(geometry):
-            for ring in geometry:
-                for point in ring:
-                    point[0] = ...
-                    point[1] = ...
-
 
         reader = copying_reader
         while (next_reader := reader()) is not None:
